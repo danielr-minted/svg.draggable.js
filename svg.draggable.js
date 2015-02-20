a9702e8fc142ed7314d1e197e770b5d2eddeb541
Daniel Rogers <danielr@minted.com>
2015-02-20 15:40:27 -0800

    Add support for local element scaling

--- svg.draggable.js
+++ svg.draggable.js
@@ -84,9 +84,9 @@
               }
 
           /* caculate new position [with rotation correction] */
-          x = element.startPosition.x + (delta.x * Math.cos(rotation) + delta.y * Math.sin(rotation))  / element.startPosition.zoom
-          y = element.startPosition.y + (delta.y * Math.cos(rotation) + delta.x * Math.sin(-rotation)) / element.startPosition.zoom
-          
+          x = element.startPosition.x + (delta.x * Math.cos(rotation) + delta.y * Math.sin(rotation))  / ( element.startPosition.zoom * element.transform('scaleX') )
+          y = element.startPosition.y + (delta.y * Math.cos(rotation) + delta.x * Math.sin(-rotation)) / ( element.startPosition.zoom * element.transform('scaleY') )
+
           /* move the element to its new position, if possible by constraint */
           if (typeof constraint === 'function') {
             var coord = constraint(x, y)
